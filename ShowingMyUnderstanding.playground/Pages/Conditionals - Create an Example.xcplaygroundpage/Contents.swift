0f0dbd2e2d772a6bd6b9d8e58ea251640737f709
Jessie Zhu <JEZhu@lcs.on.ca>
2017-11-28 13:57:50 -0500

    Done the squares and the circles that shows up randomly. I also add some certain colour into it.

--- ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -16,11 +16,19 @@
 import Cocoa
 import PlaygroundSupport
 
+let number = random(from: 0, toButNotIncluding: 2)
+
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
 // Replace this command and add your code below...
-let colorType = random(from: 0, toButNotIncluding: 5)
+if number == 0 {canvas.drawShapesWithFill
+    canvas.fillColor = Color.init(hue: 0, saturation: 150, brightness: 100, alpha: 100)
+    canvas.drawRectangle(bottomLeftX: 100, bottomLeftY: 100, width: 100, height: 100)
+} else if number == 1 {canvas.drawShapesWithFill
+    canvas.fillColor = Color.init(hue: 150, saturation: 150, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: 150, centreY: 150, width: 100, height: 100)
+}
 
 
 
