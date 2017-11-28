fd97b6bb320e22fd0422a84326971ca454726acb
Jessie Zhu <JEZhu@lcs.on.ca>
2017-11-28 13:51:44 -0500

    Done the black circle pattern.

--- ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
@@ -20,6 +20,23 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 // Replace this comment and add your code below...
+for v in stride(from: 265, through: 65, by: -100 ){
+    canvas.drawEllipse(centreX: 15, centreY: v, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 65, centreY: v, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 116, centreY: v, width: 25, height: 25);canvas.drawEllipse(centreX: 165, centreY: v, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 215, centreY: v, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 265, centreY: v, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 40, centreY: v-25, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 90, centreY: v-25, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 140, centreY: v-25, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 190, centreY: v-25, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 240, centreY: v-25, width: 25, height: 25)
+    canvas.drawEllipse(centreX: 290, centreY: v-25, width: 25, height: 25)
+    
+    
+    
+}
+    
 
 /*:
  ## Template code
